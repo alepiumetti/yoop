f6a19202a65a4167524e5ab5bd0ec6cb64512f60
Ale Piumetti <alepiumetti@hotmail.com>
2020-01-04 19:30:59 -0300

    Modificación CSS Inicio-Contacto y estructura Contacto

--- src/components/layout/contacto/Contacto.js
+++ src/components/layout/contacto/Contacto.js
@@ -7,39 +7,12 @@ export class Contacto extends Component {
     render() {
         return ( 
         <div className = "container-contacto"> 
-            <div className = "container-form">
-                <form action="/contacto" method="post">
-                    
-                    <p>
-                        ¿Tenés alguna pregunta?
-                    </p>
-                    <p className = "campos-requeridos">Los campos requeridos están marcados con un <span className="asterisco-required-contact">*</span></p>
+            <div className = "container-explicacion" >
 
-                    <p> 
-                        Nombre<span className="asterisco-required-contact">*</span> <input type = "text" name = "user_nombre" id = "nombre" required/> 
-                        &nbsp;
-                        Apellido<span className="asterisco-required-contact">*</span> <input type = "text" name = "user_apellido" id = "apellido" required/>
-                    </p> 
-                    
-                    <p> 
-                        Teléfono<span className="asterisco-required-contact">*</span> <input type = "phone" name = "user_telefono" id = "tel" required/> 
-                        &nbsp;
-                        E-mail<span className="asterisco-required-contact">*</span> <input type = "email" name = "user_email" id = "email" required/> 
-                    </p> 
-                    
-                    <p> Mensaje<span className="asterisco-required-contact">*</span>
-                        <br/>
-                        <textarea name = "mensaje" rows = "4" cols = "30" id = "user_msg" placeholder = "Escribinos.." required></textarea>
-                    </p>
-                    
-                    <p>
-                    <input className = "btn-form" type = "submit" id = "enviar" value = "Enviar" />
-                    </p>
+            </div>
 
-                    
-                    
+            <div className = "container-numeros">
 
-                </form>
             </div>
         </div>
         )
