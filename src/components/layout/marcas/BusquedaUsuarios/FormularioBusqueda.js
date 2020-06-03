7eb24293e81b680ced25913358a03ed958b6becb
Ale Piumetti <alepiumetti@hotmail.com>
2020-06-02 21:31:12 -0300

    busqueda de usuario

--- src/components/layout/marcas/BusquedaUsuarios/FormularioBusqueda.js
+++ src/components/layout/marcas/BusquedaUsuarios/FormularioBusqueda.js
@@ -4,50 +4,77 @@
 
 //React - React Router - React Fire - Firebase
 
-import React , {useState} from 'react'
-import * as firebase from 'firebase'
+import React, { useState } from "react";
+import * as firebase from "firebase";
 
 // Styles
 
-import style from "./FormularioBusquedaStyle.module.css"
+import style from "./FormularioBusquedaStyle.module.css";
 
 //Iconos
 
 //Material UI
 
-import {
-    TextField,
-    Typography,
-    Button,
-    Grid,
-} from '@material-ui/core'
+import { TextField, Typography, Button, Grid } from "@material-ui/core";
 
 //React Components
 
-import ValidacionDeUsuario from './ValidacionDeUsuario'
-
-
+import ValidacionDeUsuario from "./ValidacionDeUsuario";
 
 function FormularioBusqueda() {
+  const [nombre, setNombre] = useState(""); //se ingresa el nombre y/ apellido
+  const [dni, setDni] = useState(""); //se ingresa el DNI
+  const [getUser, setGetUser] = useState(false); // es lo que determina cuando se busca a alguien para que pida al servidor
 
-    const [nombre,setNombre] = useState();
-    const [dni,setDni] = useState();
+  const handleNombre = (e) => {
+    let valor = e.target.value;
+    setNombre(valor);
+  };
 
-    return (
-        <div className={style.container}>
-            <Grid container spacing={2}>
-                <Grid item xs={3}>
-                    <TextField label="Nombre y Apellido" value={nombre}/>
-                </Grid>
-                <Grid item xs={3}>
-                    <TextField label="DNI" value={dni}/>
-                </Grid>
-                <Grid item xs={12}>
-                    <ValidacionDeUsuario/>
-                </Grid>
-            </Grid>
-        </div>
-    )
+  const handleDNI = (e) => {
+    let valor = e.target.value;
+    setDni(valor);
+  };
+
+  const get_user = () => {
+    setGetUser(!getUser);
+  };
+
+  return (
+    <div className={style.container}>
+      {!getUser && (
+        <Grid container spacing={2}>
+          <Grid item xs={3}>
+            <TextField
+              label="Nombre y Apellido"
+              value={nombre}
+              onChange={handleNombre}
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <TextField label="DNI" value={dni} onChange={handleDNI} />
+          </Grid>
+          <Grid item xs={3}>
+            <Button variant="contained" color="default" onClick={get_user}>
+              Buscar
+            </Button>
+          </Grid>
+        </Grid>
+      )}
+      {getUser && (
+        <Grid container spacing={2}>
+          <Grid item xs={3}>
+            <ValidacionDeUsuario usuario={nombre} dni={dni} />
+          </Grid>
+          <Grid item xs={12}>
+            <Button onClick={get_user} variant="contained">
+              Volver
+            </Button>
+          </Grid>
+        </Grid>
+      )}
+    </div>
+  );
 }
 
-export default FormularioBusqueda
+export default FormularioBusqueda;
--- src/components/layout/marcas/BusquedaUsuarios/ValidacionDeUsuario.js
+++ src/components/layout/marcas/BusquedaUsuarios/ValidacionDeUsuario.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import * as firebase from "firebase";
 
-function ValidacionDeUsuario() {
-    return (
-        <div>
-            Usuario válido o no
-        </div>
-    )
+function ValidacionDeUsuario(props) {
+  const [usuario, setUsuario] = useState(props.usuario);
+  const [DNI, setDNI] = useState(props.dni);
+  const [usuarios, setUsuarios] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const listenForUsers = async () => {
+    let lista = [];
+    const itemsRef = firebase.database().ref("usuarios");
+    itemsRef.on(
+      "value",
+      function (snapshot) {
+        snapshot.forEach((child) => {
+          lista.push({
+            id: child.val().id,
+            dni: child.val().dni,
+            ingresos: child.val().ingresos,
+            nacimiento: child.val().nacimiento,
+            nivel: child.val().nivel,
+            pago: child.val().pago,
+            tel: child.val().tel,
+            nombre: child.val().nombre,
+            email: child.val().email,
+          });
+        });
+      },
+      function (errorObject) {
+        alert("Carga incompleta");
+      }
+    );
+    setUsuarios(lista);
+    setLoading(true);
+  };
+
+  useEffect(() => {
+    listenForUsers();
+  }, []);
+
+  return (
+    <div>
+      nombre: {usuario}
+      <br />
+      dni: {DNI}
+    </div>
+  );
 }
 
-export default ValidacionDeUsuario
+export default ValidacionDeUsuario;
