11c8cbf0b577cf15dccd5ac4804e4e18195f1904
Ale Piumetti <alepiumetti@hotmail.com>
2020-07-07 12:46:40 -0300

    muestra datos de ID

--- src/components/layout/marcas amigas/MarcasAmigas.jsx
+++ src/components/layout/marcas amigas/MarcasAmigas.jsx
@@ -1,11 +1,25 @@
 import React , {useState , useEffect} from 'react'
 import { useParams } from 'react-router'
-
+import * as firebase from 'firebase'
 
 function MarcasAmigas(props) {
 
     const [id , setID] = useState("")
+    const [marca , setMarca] = useState({
+        badge:"",
+        cond:"",
+        desc:"",
+        email:"",
+        fb:"",
+        ig:"",
+        tel:"",
+        vip:"",
+        web:""
+        })
 
+
+
+    const database = firebase.database()
     let url = useParams()
 
     useEffect(() => {
@@ -13,17 +27,38 @@ function MarcasAmigas(props) {
         
     }, [])
 
-    const getURL = async (url) =>{
+    const getURL = (url) =>{
         console.log(url)
-        await setID(url.id)
+        getMarca(url.id)
 
     }    
-  
+
+    const getMarca = async (id) =>{
+        let cleanID = id.slice(3)
+        let marca = ""
+        setID(cleanID)
+        database.ref(`marcas/${cleanID}`).on("value",function(snapshot){
+                marca = snapshot.val()
+                console.log(marca)
+                setMarca(marca)
+            })
+            
+            }
+        
+        console.log(marca)
 
 
     return (
         <div>
-            <p>{id}</p>     
+            <p>badge: {marca.badge}</p>
+            <p>Condiciones: {marca.cond}</p>
+            <p>descuentos: {marca.desc}</p>
+            <p>Email: {marca.email}</p> 
+            <p>Facebook: {marca.fb}</p>              
+            <p>Instagram: {marca.ig}</p>
+            <p>tel: {marca.tel}</p>
+            <p>vip: {marca.vip}</p>
+            <p>web: {marca.web}</p>
         </div>
     )
     
