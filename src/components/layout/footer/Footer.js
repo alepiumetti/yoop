5b744193f42520302151aa28b5d8da768d4788b5
Ale Piumetti <alepiumetti@hotmail.com>
2020-09-10 15:33:27 -0300

    Correcciones en router

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Link} from 'react-router-dom';
+import {BrowserRouter as Router , Link} from 'react-router-dom';
 
 import './FooterStyle.css';
 
@@ -7,9 +7,7 @@ import logoInstagram from '../../icons/001-instagram.svg' ;
 import logoFacebook from '../../icons/002-facebook.svg' ;
 import logoWhatsapp from '../../icons/003-whatsapp.svg' ;
 import logoGmail from '../../icons/004-gmail.svg' ;
-
 import downloadApp from '../../img/google-play-badge.png';
-
 import logoColor from '../../img/logo yoop.svg'
 
 
@@ -23,17 +21,17 @@ export class Footer extends Component {
                 <div className="container-footer-menu">
                     <ul>
                         <li><Link className="btn-footer" to="/" > Inicio </Link></li> 
-                        <li><Link className="btn-footer" to="/asociate"> Aasociate </Link></li>
+                        <li><Link className="btn-footer" to="/asociate"> Asociate </Link></li>
                     </ul>
                 </div>
                 <div className="container-footer-socialmedia">
                     <div> {/* Container logo Facebook */}
-                        <a href="https://facebook.com/academia.yoop">
+                        <a href="https://facebook.com/yoop.arg">
                             <img src={logoFacebook} className="logo-sm" alt="Logo facebook"/>
                         </a>
                     </div>
                     <div> {/* Container logo Instagram */}
-                        <a href="https://instagram.com/academia.yoop">
+                        <a href="https://instagram.com/yoop.ar">
                             <img src={logoInstagram} className="logo-sm" alt="logo Instagram"/>
                         </a>
                     </div>
@@ -49,7 +47,7 @@ export class Footer extends Component {
                     </div>
                 </div>
                 <div className="container-download-app">
-                    <a href="https://play.google.com/store/">
+                    <a href="">
                         <img src={downloadApp} className="img-downloadApp" alt="Descarga la app en android"/>
                     </a> 
                 </div>
