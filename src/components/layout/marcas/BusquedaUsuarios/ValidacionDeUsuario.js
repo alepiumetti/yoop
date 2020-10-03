9b9b8a22aa4cf1017c38bf1df11fc08fbcc8e5ed
Alejandro Piumetti <alepiumetti@hotmail.com>
2020-10-03 16:04:31 -0300

    Arreglo en validación de usuario y muestra de datos

--- src/components/layout/marcas/BusquedaUsuarios/ValidacionDeUsuario.js
+++ src/components/layout/marcas/BusquedaUsuarios/ValidacionDeUsuario.js
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from "react";
 import * as firebase from "firebase";
-import { Button, CircularProgress, Typography, Card, CardContent, Divider, Fade } from "@material-ui/core";
+import {
+  Button,
+  CircularProgress,
+  Typography,
+  Card,
+  CardContent,
+  Divider,
+  Fade,
+} from "@material-ui/core";
 import Moment from "react-moment";
-import {useList, useListVals} from 'react-firebase-hooks/database'
-import CheckIcon from '@material-ui/icons/Check';
-import CloseIcon from '@material-ui/icons/Close';
+import { useList, useListVals } from "react-firebase-hooks/database";
+import CheckIcon from "@material-ui/icons/Check";
+import CloseIcon from "@material-ui/icons/Close";
 
 import style from "./ValidacionDelUsuarioStyle.module.css";
 
@@ -15,113 +23,106 @@ function ValidacionDeUsuario(props) {
   const [nombre, setNombre] = useState(props.usuario); //Nombre del usuario que se busca
   const [noUser, setNoUser] = useState(false); //Handler de contenido cuando el usuario no existe
 
-  const [values,loading,error] = useListVals(firebase.database().ref("usuarios"))
-  console.log(values)
-  console.log(loading)
-  console.log(error)
+  const [values, loading, error] = useListVals(
+    firebase.database().ref("usuarios")
+  );
 
   const buscarUsuario = () => {
     let lista = values; //Guarda el parámetro en una variable
     let nombreState = nombre.toLowerCase(); //Pasa el nombre del input a minusculas
 
     if (nombre !== "" && DNI === "") {
-
-      lista.find((element)=>{
-        let nombreElement = element.nombre.toLowerCase();
-        if ((nombreElement === nombreState)) {
-          setUsuario(element);
-          setPago(true)
-        }else {
-             setNoUser(true);
-             console.log("notUser1");
-            } 
-      })
-    } else if (DNI !== "" && nombre === "") {
-      lista.find((element)=>{
-        console.log(element)
-        let dniElement = element.dni;
-        if ((dniElement === DNI)) {
+      lista.find(function (element) {
+        let nombreElement;
+        if (element.nombre === undefined) {
+        } else {
+          nombreElement = element.nombre.toLowerCase();
+        }
+        if (nombreElement === nombreState) {
           setUsuario(element);
           setPago(true);
-        }else {
+        } else {
           setNoUser(true);
-          console.log("notUser2");
-         } 
-      })
+        }
+      });
+    } else if (DNI !== "" && nombre === "") {
+      lista.find((element) => {
+        let dniElement = element.dni;
+        if (dniElement === DNI) {
+          setUsuario(element);
+          setPago(true);
+        } else {
+          setNoUser(true);
+        }
+      });
     } else if (DNI !== "" && nombre !== "") {
-      lista.find((element)=>{
+      lista.find((element) => {
         let nombreElement = element.nombre.toLowerCase();
         let dniElement = element.dni;
-        if ((nombreElement === nombreState) && (dniElement === DNI)) {
+        if (nombreElement === nombreState && dniElement === DNI) {
           setUsuario(element);
-          setPago(true)
+          setPago(true);
         } else {
           alert("Los datos no coinciden con ningun usuario");
         }
-      })
+      });
     } else {
       alert("Debe ingresar el nombre o DNI del asociado");
     }
   };
   useEffect(() => {
-    buscarUsuario()
-  }, [loading])
+    buscarUsuario();
+  }, [loading]);
 
   return (
     <div>
       {loadPago && usuario.pago && (
         <Fade in={loadPago}>
           <div className={style.containerCard}>
-            
-              <div className={style.containerSuccess}>
-                <CheckIcon style={{color:"#FFF",fontSize:"120px"}}/>
-              </div>
-              <div className={style.containerUser}>
-                <Typography variant="h5">{usuario.nombre}</Typography>
-                <Divider style={{margin:"5px 0px 5px 0px"}}/>
-                <Typography variant="body1">DNI: {usuario.dni}</Typography>
-                <Typography variant="body1">Teléfono: {usuario.tel}</Typography>
-                <Button onClick={props.getUser} variant="contained">
-                  Volver
-                </Button>
-              </div>
+            <div className={style.containerSuccess}>
+              <CheckIcon style={{ color: "#FFF", fontSize: "120px" }} />
+            </div>
+            <div className={style.containerUser}>
+              <Typography variant="h5">{usuario.nombre}</Typography>
+              <Divider style={{ margin: "5px 0px 5px 0px" }} />
+              <Typography variant="body1">DNI: {usuario.dni}</Typography>
+              <Typography variant="body1">Teléfono: {usuario.tel}</Typography>
+              <Button onClick={props.getUser} variant="contained">
+                Volver
+              </Button>
+            </div>
           </div>
         </Fade>
-
       )}
       {loadPago && !usuario.pago && (
         <Fade in={loadPago}>
           <div className={style.containerCard}>
-          <div className={style.containerFailed}>
-            <CloseIcon style={{color:"#FFF",fontSize:"120px"}}/>
+            <div className={style.containerFailed}>
+              <CloseIcon style={{ color: "#FFF", fontSize: "120px" }} />
+            </div>
+            <div className={style.containerUser}>
+              <Typography variant="h5">{usuario.nombre}</Typography>
+              <Divider style={{ margin: "5px 0px 5px 0px" }} />
+              <Typography variant="body1">DNI: {usuario.dni}</Typography>
+              <Typography variant="body1">Teléfono: {usuario.tel}</Typography>
+              <Button onClick={props.getUser} variant="contained">
+                Volver
+              </Button>
+            </div>
           </div>
-          <div className={style.containerUser}>
-            <Typography variant="h5">{usuario.nombre}</Typography>
-            <Divider style={{margin:"5px 0px 5px 0px"}}/>
-            <Typography variant="body1">DNI: {usuario.dni}</Typography>
-            <Typography variant="body1">Teléfono: {usuario.tel}</Typography>
-            <Button onClick={props.getUser} variant="contained">
-              Volver
-            </Button>
-          </div>
-        </div>
-      </Fade>
+        </Fade>
       )}
 
-      {noUser && !loadPago &&(
+      {noUser && !loadPago && (
         <div>
           <p>El usuario no existe o los datos son erroneos</p>
           <Button onClick={props.getUser} variant="contained">
             Volver
           </Button>
         </div>
-        
       )}
 
-      {loading && (
-        <CircularProgress/>
-      )}
-      
+      {loading && <CircularProgress />}
     </div>
   );
 }
--- src/components/layout/marcas/datosMarcas/Informacion.jsx
+++ src/components/layout/marcas/datosMarcas/Informacion.jsx
@@ -1,7 +1,16 @@
 import React, { useEffect } from "react";
 import * as firebase from "firebase";
 import { useObject } from "react-firebase-hooks/database";
-import { TextField, Typography } from "@material-ui/core";
+import { Grid, TextField, Typography } from "@material-ui/core";
+
+const style = {
+  textField: { padding: "10px" },
+  tituloMarca: {
+    paddingTop: "10px",
+    paddingBottom: "10px",
+    textTransform: "uppercase",
+  },
+};
 
 function Informacion() {
   let user = firebase.auth().currentUser;
@@ -10,21 +19,83 @@ function Informacion() {
     firebase.database().ref(firebase.database().ref(`usuarios/${user.uid}`))
   );
 
+  useEffect(() => {}, [loading]);
+
+  return (
+    <div style={style.container}>
+      {!loading && (
+        <Grid container spacing={2}>
+          <Grid item xs="12">
+            <Typography style={style.tituloMarca} variant="h3">
+              {usuario.val().nombre}{" "}
+            </Typography>
+          </Grid>
+          <Datos id={usuario.val().marca} />
+        </Grid>
+      )}
+    </div>
+  );
+}
+
+function Datos(props) {
   const [marca, loadingMarca, errorMarca] = useObject(
-    firebase.database().ref(`marcas/${usuario.val().marca}`)
+    firebase.database().ref(`marcas/${props.id}`)
   );
 
-  useEffect(() => {}, [loading, loadingMarca]);
+  useEffect(() => {}, [loadingMarca]);
 
   return (
     <div>
-      {!loading && !loadingMarca && (
+      {!loadingMarca && (
         <div>
-          <Typography variant="h3">{usuario.val().nombre} </Typography>
           <TextField
+            style={style.textField}
             disabled
             value={marca.val().badge}
+            label="Cantidad de descuento"
+          ></TextField>
+          <TextField
+            style={style.textField}
+            disabled
+            value={marca.val().desc}
             label="Descuento"
+          ></TextField>
+          <TextField
+            style={style.textField}
+            disabled
+            value={marca.val().cond}
+            label="Condición"
+            multiline
+          ></TextField>
+          <TextField
+            style={style.textField}
+            disabled
+            value={marca.val().ig}
+            label="Instagram"
+          ></TextField>
+          <TextField
+            style={style.textField}
+            disabled
+            value={marca.val().fb}
+            label="Facebook"
+          ></TextField>
+          <TextField
+            style={style.textField}
+            disabled
+            value={marca.val().web}
+            label="Web"
+          ></TextField>
+          <TextField
+            style={style.textField}
+            disabled
+            value={marca.val().dir}
+            label="Dirección"
+          ></TextField>
+          <TextField
+            style={style.textField}
+            disabled
+            value={marca.val().cel}
+            label="Teléfono"
           ></TextField>
         </div>
       )}
